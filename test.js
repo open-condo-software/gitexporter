cf28161b6bed1145a4ed298303d2652c7228e108
Pahaz <pahaz.white@gmail.com>
2021-12-05 20:12:39 +0500

    test.js: strictly check git commit history `git log -p`

--- test.js
+++ test.js
@@ -5,10 +5,15 @@ const exec = util.promisify(require('child_process').exec)
 
 const writeFileAtomic = require('write-file-atomic')
 
-async function run (command) {
+async function run (command, expectStdoutLines = null) {
     const { stdout, stderr } = await exec(command)
-    console.log(command, stdout)
+    console.log('RUN:', command)
+    console.log(stdout)
+    if (stderr) console.error('ERROR:', stderr)
     expect(stderr).toBe('')
+    if (expectStdoutLines) {
+        expect(stdout.split('\n').map(x => x.trim()).filter(x => !!x)).toEqual(expectStdoutLines)
+    }
     return { stdout, stderr }
 }
 
@@ -47,148 +52,110 @@ async function prepareGitRepo (folder) {
     await exec(`GIT_COMMITTER_DATE="2010-01-01T22:05:00Z" git -C ${folder} -c user.name="Name2" -c user.email=user2@example.com commit --author='User <user@example.com>' --date "2005-09-07T23:13:13Z" -am 'add script!'`)
 }
 
+const DEFAULT_PREPARE_GIT_REPO_HISTORY = [
+    'commit 43a8998c57a1885fb9bb4ae8342b2e8a9285f002',
+    'Author: User <user@example.com>',
+    'Date:   Wed Sep 7 23:13:13 2005 +0000',
+    'add script!',
+    'diff --git a/bin/script.js b/bin/script.js',
+    'new file mode 100755',
+    'index 0000000..ca29b27',
+    '--- /dev/null',
+    '+++ b/bin/script.js',
+    '@@ -0,0 +1,2 @@',
+    '+#!/usr/bin/env node',
+    '+console.log(911)',
+    'commit a9384415955e78b0adfd00e5fb95b99eff97138c',
+    'Author: User <user@example.com>',
+    'Date:   Sun Aug 7 23:13:13 2005 +0000',
+    'another file',
+    'diff --git a/sTest.txt b/sTest.txt',
+    'new file mode 100644',
+    'index 0000000..6dbb898',
+    '--- /dev/null',
+    '+++ b/sTest.txt',
+    '@@ -0,0 +1 @@',
+    '+Initial text',
+    '\\ No newline at end of file',
+    'commit dded478c4d2bf21367a88d7e9bb2b6ea18eb3c50',
+    'Author: User <user@example.com>',
+    'Date:   Thu Jul 7 23:13:13 2005 +0000',
+    'create link',
+    'diff --git a/Test.txt.link b/Test.txt.link',
+    'new file mode 120000',
+    'index 0000000..d7da186',
+    '--- /dev/null',
+    '+++ b/Test.txt.link',
+    '@@ -0,0 +1 @@',
+    '+Test.txt',
+    '\\ No newline at end of file',
+    'commit ee01df4e6cc73e4210e87c94b853a96103ca02c2',
+    'Author: User <user@example.com>',
+    'Date:   Tue Jun 7 23:13:13 2005 +0000',
+    'rename file',
+    'diff --git a/test.txt b/Test.txt',
+    'similarity index 100%',
+    'rename from test.txt',
+    'rename to Test.txt',
+    'commit d0dc86bea4f548db93905b71da1c6915594bfd5b',
+    'Author: User <user@example.com>',
+    'Date:   Sat May 7 23:13:13 2005 +0000',
+    'change initial text',
+    'diff --git a/test.txt b/test.txt',
+    'index 6dbb898..41c4a21 100644',
+    '--- a/test.txt',
+    '+++ b/test.txt',
+    '@@ -1 +1 @@',
+    '-Initial text',
+    '\\ No newline at end of file',
+    '+Changed text',
+    '\\ No newline at end of file',
+    'commit c731fd997376f68806c5cbe100edc7000acb75db',
+    'Author: User <user@example.com>',
+    'Date:   Thu Apr 7 22:13:13 2005 +0000',
+    'initial commit',
+    'diff --git a/test.txt b/test.txt',
+    'new file mode 100644',
+    'index 0000000..6dbb898',
+    '--- /dev/null',
+    '+++ b/test.txt',
+    '@@ -0,0 +1 @@',
+    '+Initial text',
+    '\\ No newline at end of file',
+]
+
 test('prepareGitRepo()', async () => {
     const folder = 'ignore.prepare-git-repo'
     await prepareGitRepo(folder)
 
-    {
-        const {
-            stdout,
-            stderr,
-        } = await exec(`git -C ${folder} log --pretty='format:%H %aI "%an" <%ae> %cI "%cn" <%ce> %s'`)
-        expect(stderr).toBe('')
-        expect(stdout).toBe([
-            '43a8998c57a1885fb9bb4ae8342b2e8a9285f002 2005-09-07T23:13:13+00:00 "User" <user@example.com> 2010-01-01T22:05:00+00:00 "Name2" <user2@example.com> add script!',
-            'a9384415955e78b0adfd00e5fb95b99eff97138c 2005-08-07T23:13:13+00:00 "User" <user@example.com> 2010-01-01T22:04:00+00:00 "Name2" <user2@example.com> another file',
-            'dded478c4d2bf21367a88d7e9bb2b6ea18eb3c50 2005-07-07T23:13:13+00:00 "User" <user@example.com> 2010-01-01T22:03:00+00:00 "Name2" <user2@example.com> create link',
-            'ee01df4e6cc73e4210e87c94b853a96103ca02c2 2005-06-07T23:13:13+00:00 "User" <user@example.com> 2010-01-01T22:02:00+00:00 "Name2" <user2@example.com> rename file',
-            'd0dc86bea4f548db93905b71da1c6915594bfd5b 2005-05-07T23:13:13+00:00 "User" <user@example.com> 2010-01-01T22:01:00+00:00 "Name2" <user2@example.com> change initial text',
-            'c731fd997376f68806c5cbe100edc7000acb75db 2005-04-07T22:13:13+00:00 "User" <user@example.com> 2010-01-01T22:00:00+00:00 "Name2" <user2@example.com> initial commit',
-        ].join('\n'))
-    }
-    {
-        const { stdout, stderr } = await exec(`git -C ${folder} log -p`)
-        expect(stderr).toBe('')
-        expect(stdout).toBe(`commit 43a8998c57a1885fb9bb4ae8342b2e8a9285f002
-Author: User <user@example.com>
-Date:   Wed Sep 7 23:13:13 2005 +0000
+    await run(`git -C ${folder} log --pretty='format:%H %aI "%an" <%ae> %cI "%cn" <%ce> %s'`, [
+        '43a8998c57a1885fb9bb4ae8342b2e8a9285f002 2005-09-07T23:13:13+00:00 "User" <user@example.com> 2010-01-01T22:05:00+00:00 "Name2" <user2@example.com> add script!',
+        'a9384415955e78b0adfd00e5fb95b99eff97138c 2005-08-07T23:13:13+00:00 "User" <user@example.com> 2010-01-01T22:04:00+00:00 "Name2" <user2@example.com> another file',
+        'dded478c4d2bf21367a88d7e9bb2b6ea18eb3c50 2005-07-07T23:13:13+00:00 "User" <user@example.com> 2010-01-01T22:03:00+00:00 "Name2" <user2@example.com> create link',
+        'ee01df4e6cc73e4210e87c94b853a96103ca02c2 2005-06-07T23:13:13+00:00 "User" <user@example.com> 2010-01-01T22:02:00+00:00 "Name2" <user2@example.com> rename file',
+        'd0dc86bea4f548db93905b71da1c6915594bfd5b 2005-05-07T23:13:13+00:00 "User" <user@example.com> 2010-01-01T22:01:00+00:00 "Name2" <user2@example.com> change initial text',
+        'c731fd997376f68806c5cbe100edc7000acb75db 2005-04-07T22:13:13+00:00 "User" <user@example.com> 2010-01-01T22:00:00+00:00 "Name2" <user2@example.com> initial commit',
+    ])
 
-    add script!
-
-diff --git a/bin/script.js b/bin/script.js
-new file mode 100755
-index 0000000..ca29b27
---- /dev/null
-+++ b/bin/script.js
-@@ -0,0 +1,2 @@
-+#!/usr/bin/env node
-+console.log(911)
-
-commit a9384415955e78b0adfd00e5fb95b99eff97138c
-Author: User <user@example.com>
-Date:   Sun Aug 7 23:13:13 2005 +0000
-
-    another file
-
-diff --git a/sTest.txt b/sTest.txt
-new file mode 100644
-index 0000000..6dbb898
---- /dev/null
-+++ b/sTest.txt
-@@ -0,0 +1 @@
-+Initial text
-\\ No newline at end of file
-
-commit dded478c4d2bf21367a88d7e9bb2b6ea18eb3c50
-Author: User <user@example.com>
-Date:   Thu Jul 7 23:13:13 2005 +0000
-
-    create link
-
-diff --git a/Test.txt.link b/Test.txt.link
-new file mode 120000
-index 0000000..d7da186
---- /dev/null
-+++ b/Test.txt.link
-@@ -0,0 +1 @@
-+Test.txt
-\\ No newline at end of file
-
-commit ee01df4e6cc73e4210e87c94b853a96103ca02c2
-Author: User <user@example.com>
-Date:   Tue Jun 7 23:13:13 2005 +0000
-
-    rename file
-
-diff --git a/test.txt b/Test.txt
-similarity index 100%
-rename from test.txt
-rename to Test.txt
-
-commit d0dc86bea4f548db93905b71da1c6915594bfd5b
-Author: User <user@example.com>
-Date:   Sat May 7 23:13:13 2005 +0000
-
-    change initial text
-
-diff --git a/test.txt b/test.txt
-index 6dbb898..41c4a21 100644
---- a/test.txt
-+++ b/test.txt
-@@ -1 +1 @@
--Initial text
-\\ No newline at end of file
-+Changed text
-\\ No newline at end of file
-
-commit c731fd997376f68806c5cbe100edc7000acb75db
-Author: User <user@example.com>
-Date:   Thu Apr 7 22:13:13 2005 +0000
-
-    initial commit
-
-diff --git a/test.txt b/test.txt
-new file mode 100644
-index 0000000..6dbb898
---- /dev/null
-+++ b/test.txt
-@@ -0,0 +1 @@
-+Initial text
-\\ No newline at end of file
-`)
-    }
-})
-
-test.skip('gitexporter config.json', async () => {
-    await exec(`rm -rf  ignore.default`)
-    await exec(`rm -rf  ignore.default.log.json`)
-    await run(`node --unhandled-rejections=strict index.js config.json`)
-    const file1 = fs.readFileSync('index.js', { encoding: 'utf-8' })
-    const file2 = fs.readFileSync('ignore.default/index.js', { encoding: 'utf-8' })
-    expect(file1).toBe(file2)
+    await run(`git -C ${folder} log -p`, DEFAULT_PREPARE_GIT_REPO_HISTORY)
 })
 
 test('gitexporter allowed paths', async () => {
+    const folder = 'ignore.allowed-paths'
     const config = `{
   "forceReCreateRepo": true,
-  "targetRepoPath": "ignore.allowed-paths-target",
-  "sourceRepoPath": "ignore.allowed-paths",
+  "targetRepoPath": "${folder}-target",
+  "sourceRepoPath": "${folder}",
   "allowedPaths": ["test.txt"]
 }`
-    await run('rm -rf ignore.allowed-paths*')
-    await prepareGitRepo('ignore.allowed-paths')
-    await writeFileAtomic('ignore.allowed-paths.config.json', config)
-    await run(`node --unhandled-rejections=strict index.js ignore.allowed-paths.config.json`)
+    await run(`rm -rf ${folder}*`)
+    await prepareGitRepo(`${folder}`)
+    await writeFileAtomic(`${folder}.config.json`, config)
+    await run(`node --unhandled-rejections=strict index.js ${folder}.config.json`)
 
-    const { stdout, stderr } = await exec(`ls -a ignore.allowed-paths-target`)
-    expect(stderr).toBe('')
-    expect(stdout).toBe(`.
-..
-.git
-Test.txt
-`)
+    await run(`ls -a ${folder}-target`, ['.', '..', '.git', 'Test.txt'])
 
-    const logs = JSON.parse(fs.readFileSync('ignore.allowed-paths-target.log.json', { encoding: 'utf-8' }))
+    const logs = JSON.parse(fs.readFileSync(`${folder}-target.log.json`, { encoding: 'utf-8' }))
     expect(logs.paths).toEqual([
         'test.txt',
         'Test.txt',
@@ -201,32 +168,78 @@ Test.txt
         'test.txt',
         'Test.txt',
     ])
+
+    // NOTE: probably may have some differences on register sensitive filepath filesystems?!
+    await run(`git -C ${folder}-target log -p`, [
+        'commit 6599d82704d3906cfd707a84cf99c4a510fada69',
+        'Author: User <user@example.com>',
+        'Date:   Wed Sep 7 23:13:13 2005 +0000',
+        'add script!',
+        'commit 789e226321099850e0381797fb8732ea3d9a95f0',
+        'Author: User <user@example.com>',
+        'Date:   Sun Aug 7 23:13:13 2005 +0000',
+        'another file',
+        'commit 93c00f434da4ac6853abd1ed389b6051ce198501',
+        'Author: User <user@example.com>',
+        'Date:   Thu Jul 7 23:13:13 2005 +0000',
+        'create link',
+        'commit 4621bc9bbafbbf07d635751fe622906cd9451f81',
+        'Author: User <user@example.com>',
+        'Date:   Tue Jun 7 23:13:13 2005 +0000',
+        'rename file',
+        'diff --git a/test.txt b/test.txt',
+        'deleted file mode 100644',
+        'index 41c4a21..0000000',
+        '--- a/test.txt',
+        '+++ /dev/null',
+        '@@ -1 +0,0 @@',
+        '-Changed text',
+        '\\ No newline at end of file',
+        'commit d0dc86bea4f548db93905b71da1c6915594bfd5b',
+        'Author: User <user@example.com>',
+        'Date:   Sat May 7 23:13:13 2005 +0000',
+        'change initial text',
+        'diff --git a/test.txt b/test.txt',
+        'index 6dbb898..41c4a21 100644',
+        '--- a/test.txt',
+        '+++ b/test.txt',
+        '@@ -1 +1 @@',
+        '-Initial text',
+        '\\ No newline at end of file',
+        '+Changed text',
+        '\\ No newline at end of file',
+        'commit c731fd997376f68806c5cbe100edc7000acb75db',
+        'Author: User <user@example.com>',
+        'Date:   Thu Apr 7 22:13:13 2005 +0000',
+        'initial commit',
+        'diff --git a/test.txt b/test.txt',
+        'new file mode 100644',
+        'index 0000000..6dbb898',
+        '--- /dev/null',
+        '+++ b/test.txt',
+        '@@ -0,0 +1 @@',
+        '+Initial text',
+        '\\ No newline at end of file',
+    ])
 })
 
 test('gitexporter ignored paths', async () => {
+    const folder = 'ignore.ignored-paths'
     const config = `{
   "forceReCreateRepo": true,
-  "targetRepoPath": "ignore.ignored-paths-target",
-  "sourceRepoPath": "ignore.ignored-paths",
+  "targetRepoPath": "${folder}-target",
+  "sourceRepoPath": "${folder}",
   "ignoredPaths": ["test.txt"]
 }`
 
-    await run('rm -rf ignore.ignored-paths*')
-    await prepareGitRepo('ignore.ignored-paths')
-    await writeFileAtomic('ignore.ignored-paths.config.json', config)
-    await run(`node --unhandled-rejections=strict index.js ignore.ignored-paths.config.json`)
+    await run(`rm -rf ${folder}*`)
+    await prepareGitRepo(`${folder}`)
+    await writeFileAtomic(`${folder}.config.json`, config)
+    await run(`node --unhandled-rejections=strict index.js ${folder}.config.json`)
 
-    const { stdout, stderr } = await exec(`ls -a ignore.ignored-paths-target`)
-    expect(stderr).toBe('')
-    expect(stdout).toBe(`.
-..
-.git
-Test.txt.link
-bin
-sTest.txt
-`)
+    await run(`ls -a ${folder}-target`, ['.', '..', '.git', 'Test.txt.link', 'bin', 'sTest.txt'])
 
-    const logs = JSON.parse(fs.readFileSync('ignore.ignored-paths-target.log.json', { encoding: 'utf-8' }))
+    const logs = JSON.parse(fs.readFileSync(`${folder}-target.log.json`, { encoding: 'utf-8' }))
     expect(logs.paths).toEqual([
         'test.txt',
         'Test.txt',
@@ -244,32 +257,78 @@ sTest.txt
         'Test.txt.link',
         'sTest.txt',
         'bin/script.js',
+    ])
+
+    await run(`git -C ${folder}-target log -p`, [
+        'commit 57662116ed2bfa91dadce6eff592866add37484a',
+        'Author: User <user@example.com>',
+        'Date:   Wed Sep 7 23:13:13 2005 +0000',
+        'add script!',
+        'diff --git a/bin/script.js b/bin/script.js',
+        'new file mode 100755',
+        'index 0000000..ca29b27',
+        '--- /dev/null',
+        '+++ b/bin/script.js',
+        '@@ -0,0 +1,2 @@',
+        '+#!/usr/bin/env node',
+        '+console.log(911)',
+        'commit b7278d18a44009568e418c3c133086ab222807d7',
+        'Author: User <user@example.com>',
+        'Date:   Sun Aug 7 23:13:13 2005 +0000',
+        'another file',
+        'diff --git a/sTest.txt b/sTest.txt',
+        'new file mode 100644',
+        'index 0000000..6dbb898',
+        '--- /dev/null',
+        '+++ b/sTest.txt',
+        '@@ -0,0 +1 @@',
+        '+Initial text',
+        '\\ No newline at end of file',
+        'commit 6b2f22e047180853f9927bffc8da3015f82fe568',
+        'Author: User <user@example.com>',
+        'Date:   Thu Jul 7 23:13:13 2005 +0000',
+        'create link',
+        'diff --git a/Test.txt.link b/Test.txt.link',
+        'new file mode 120000',
+        'index 0000000..d7da186',
+        '--- /dev/null',
+        '+++ b/Test.txt.link',
+        '@@ -0,0 +1 @@',
+        '+Test.txt',
+        '\\ No newline at end of file',
+        'commit f995e7171c59eca6d1c664cfa4b074a117109cf5',
+        'Author: User <user@example.com>',
+        'Date:   Tue Jun 7 23:13:13 2005 +0000',
+        'rename file',
+        'commit 8e4da0cc03de0ae8f434a878a00eb9d600d3de56',
+        'Author: User <user@example.com>',
+        'Date:   Sat May 7 23:13:13 2005 +0000',
+        'change initial text',
+        'commit 2ddd8f9d41399241dec8f4dce64e6365335baa1f',
+        'Author: User <user@example.com>',
+        'Date:   Thu Apr 7 22:13:13 2005 +0000',
+        'initial commit',
     ])
 })
 
 test('gitexporter ignored and allowed paths', async () => {
+    const folder = 'ignore.ignored-allowed-paths'
     const config = `{
   "forceReCreateRepo": true,
-  "targetRepoPath": "ignore.ignored-allowed-paths-target",
-  "sourceRepoPath": "ignore.ignored-allowed-paths",
+  "targetRepoPath": "${folder}-target",
+  "sourceRepoPath": "${folder}",
   "ignoredPaths": ["*.txt"],
   "allowedPaths": ["*.js"]
 }`
 
-    await run('rm -rf ignore.ignored-allowed-paths*')
-    await prepareGitRepo('ignore.ignored-allowed-paths')
-    await writeFileAtomic('ignore.ignored-allowed-paths.config.json', config)
-    await run(`node --unhandled-rejections=strict index.js ignore.ignored-allowed-paths.config.json`)
+    await run(`rm -rf ${folder}*`)
+    await prepareGitRepo(`${folder}`)
+    await writeFileAtomic(`${folder}.config.json`, config)
+    await run(`node --unhandled-rejections=strict index.js ${folder}.config.json`)
 
-    const { stdout, stderr } = await exec(`ls -a ignore.ignored-allowed-paths-target`)
-    expect(stderr).toBe('')
-    expect(stdout).toBe(`.
-..
-.git
-bin
-`)
+    await run(`ls -a ${folder}-target`, ['.', '..', '.git', 'bin'])
 
-    const logs = JSON.parse(fs.readFileSync('ignore.ignored-allowed-paths-target.log.json', { encoding: 'utf-8' }))
+    const logs = JSON.parse(fs.readFileSync(`${folder}-target.log.json`, { encoding: 'utf-8' }))
     expect(logs.paths).toEqual([
         'test.txt',
         'Test.txt',
@@ -284,5 +343,40 @@ bin
     ])
     expect(logs.allowedPaths).toEqual([
         'bin/script.js',
+    ])
+
+    await run(`git -C ${folder}-target log -p`, [
+        'commit 7917736a48e49c96b1cdc50847f80885df814300',
+        'Author: User <user@example.com>',
+        'Date:   Wed Sep 7 23:13:13 2005 +0000',
+        'add script!',
+        'diff --git a/bin/script.js b/bin/script.js',
+        'new file mode 100755',
+        'index 0000000..ca29b27',
+        '--- /dev/null',
+        '+++ b/bin/script.js',
+        '@@ -0,0 +1,2 @@',
+        '+#!/usr/bin/env node',
+        '+console.log(911)',
+        'commit dd10c6696fe79dc6c0f403d612206a6dd41f45d3',
+        'Author: User <user@example.com>',
+        'Date:   Sun Aug 7 23:13:13 2005 +0000',
+        'another file',
+        'commit d6f6abe8b06b56013246276299a9671213671ee0',
+        'Author: User <user@example.com>',
+        'Date:   Thu Jul 7 23:13:13 2005 +0000',
+        'create link',
+        'commit f995e7171c59eca6d1c664cfa4b074a117109cf5',
+        'Author: User <user@example.com>',
+        'Date:   Tue Jun 7 23:13:13 2005 +0000',
+        'rename file',
+        'commit 8e4da0cc03de0ae8f434a878a00eb9d600d3de56',
+        'Author: User <user@example.com>',
+        'Date:   Sat May 7 23:13:13 2005 +0000',
+        'change initial text',
+        'commit 2ddd8f9d41399241dec8f4dce64e6365335baa1f',
+        'Author: User <user@example.com>',
+        'Date:   Thu Apr 7 22:13:13 2005 +0000',
+        'initial commit',
     ])
 })
